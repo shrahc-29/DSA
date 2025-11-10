bb440245baa9ff79d80b0da104247b8bd2ff2458
shrahc-29 <shriyabagade3@gmail.com>
2025-11-11 01:30:52 +0530

    Update Assign9b.cpp

--- Assign9b.cpp
+++ Assign9b.cpp
@@ -1,174 +1,85 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-class CircularQueue
+class CQ
 {
-    public:
-        int customers[4];
-        int front;
-        int rear;
+        int *arr;
+        int currsize, cap;
+        int f=0;
+        int r=-1;
 
-        CircularQueue()
-        {
-            front=-1;
-            rear=-1;
-        }
+        public:
 
-        int isEmpty()
-        {
-            if(rear==-1)
+            CQ(int size)
             {
-                return 1;
+                cap=size;
+                currsize=0;
+                arr=new int[cap];
+                f=0;
+                r=-1;
             }
-            else
-            {
-                return 0;
-            }
-        }
 
-        int isFull()
-        {
-            if((rear+1)%4==front)
+            void enqueue(int data)
             {
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-    
-        void addCustomer()
-        {
-            int token;
-            cout<<"Enter the token number:";
-            cin>>token;
-    
-            if(isFull()==1)
-            {
-                cout<<"The queue is full.\n";
-            }
-            else if(isEmpty()==1)
-            {
-                front=(front+1)%4;
-                rear=(rear+1)%4;
-                customers[rear]=token;
-            }
-            else
-            {
-                rear=(rear+1)%4;
-                customers[rear]=token;
-            }
-        }
-    
-        void serveCustomer()
-        {
-            if(isEmpty()==1)
-            {
-                cout<<"The queue is empty."<<endl;
-            }
-            else if(front==rear)
-            {
-                cout<<"Token no.:"<<customers[front]<<endl;
-                front=-1;
-                rear=-1;
-            }
-            else
-            {
-                cout<<"Token no.:"<<customers[front]<<endl;
-                front=(front+1)%4;
-            }
-        }
-
-        void closeCounter()
-        {
-            cout<<"Closing counter..."<<endl;
-            while(!isEmpty())
-            {
-                serveCustomer();
-            }
-            cout<<"Counter closed.\n";
-        }
-
-        void viewQueue()
-        {
-            int pointer=front;
-            while(pointer!=-1)
-            {
-                if(front==rear)
+                if(currsize==cap)
                 {
-                    cout<<"Token no.:"<<customers[pointer]<<endl;
-                    pointer=-1;
+                    cout<<"Full"<<endl;
+                    return;
+                }
+                r=(r+1)%cap;
+                arr[r]=data;
+                currsize++;
+            }
+
+
+            void dequeue()
+            {
+                if(empty()==true)
+                {
+                    cout<<"Empty"<<endl;
+                    return;
+                }
+                f=(f+1)%cap;
+                currsize--;
+
+            }
+
+            int front()
+            {
+                return arr[f];
+            }
+
+
+            bool empty()
+            {
+                if(currsize==0)
+                {
+                    return true;
                 }
                 else
                 {
-                    cout<<"Token no.:"<<customers[pointer]<<endl;
-                    if(pointer==rear)
-                    {
-                        pointer=-1;
-                    }
-                    else
-                    {
-                        pointer=(pointer+1)%4;
-                    }
+                    return false;
                 }
             }
-            if(isEmpty()==1)
-            {
-                cout<<"The queue is empty."<<endl;
-                pointer=-1;
-            }
-        }
-};
 
+            void display()
+            {
+                for(int i=0;i<cap;i++)
+                {
+                    cout<<arr[i]<<" "<<endl;
+
+                }
+            }
+};
 
 int main()
 {
-    CircularQueue queue;
-    int option;
+    CQ cq(4);
+    int data;
 
-    do
-    {
-        cout<<"\nSupermarket Checkout Counter:\n";
-        cout<<"1.Customer arrival\n";
-        cout<<"2.Customer checkout\n";
-        cout<<"3.Close Checkout Counter\n";
-        cout<<"4.View Counter\n";
-        cout<<"0.Exit\n";
 
-        cout<<"Enter your choice:";
-        cin>>option;
 
-        switch(option)
-        {
-            case 1:
-                queue.addCustomer();
-                break;
-
-            case 2:
-                queue.serveCustomer();
-                break;
-    
-            case 3:
-                queue.closeCounter();
-                break;
-
-            case 4:
-                queue.viewQueue();
-                break;
-
-            case 0:
-                cout<<"Exiting..."<<endl;
-                break;
-    
-            default:
-                cout<<"Invalid choice!"<<endl;
-        }
-    }
-    while(option!=0);
-
-    return 0;
 }
-
 
